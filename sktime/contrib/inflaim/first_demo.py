8555f21bb14c6ff01d849db762bbc11fadedda4e
Tony Bagnall <ajb@uea.ac.uk>
2021-06-30 14:35:55 +0100

    Update first_demo.py

--- sktime/contrib/inflaim/first_demo.py
+++ sktime/contrib/inflaim/first_demo.py
@@ -9,15 +9,19 @@ from sktime.utils.data_io import load_from_tsfile_to_dataframe
 from sktime.classification.kernel_based import Arsenal
 from sktime.utils.data_processing import from_nested_to_2d_array
 from sklearn.metrics import accuracy_score
-from sktime.clustering import (TimeSeriesKMeans,TimeSeriesKMedoids)
+#from sktime.clustering import (TimeSeriesKMeans,TimeSeriesKMedoids)
 
 def clusteringExample():
     clusterer1 = TimeSeriesKMeans(n_clusters=5, max_iter=50,
                                   averaging_algorithm="mean")
     clusterer2 = TimeSeriesKMedoids()
-    X, y = load_from_tsfile_to_dataframe("crp.ts");
+    X, y = load_from_tsfile_to_dataframe("C:\\Temp\\NOAR\\crp.ts");
     clusterer1.fit(X)
     c = clusterer1.predict(X)
+
+
+
+
 
 def classificationExample():
     trainX, trainY= load_from_tsfile_to_dataframe("C:\\Temp\\NOAR\\crp_TRAIN.ts");
@@ -26,12 +30,6 @@ def classificationExample():
     afc.fit(trainX,trainY)
     preds = afc.predict(testX)
     print(" Test set accuracy the Arsenal",accuracy_score(preds,testY))
-
-
-
-
-
-
 
     npTrain=from_nested_to_2d_array(trainX, return_numpy=True)
     npTest=from_nested_to_2d_array(testX, return_numpy=True)
